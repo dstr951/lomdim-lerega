addc7660d76a758f94bca9416c80d814fc9e094a
Daniel Goldstein <dstr159@gmail.com>
2023-10-10 16:56:39 +0300

    fix routes #2

--- webClient/src/Routes.js
+++ webClient/src/Routes.js
@@ -1,20 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Routes as Router, Route, Link } from "react-router-dom";
 import App from "./App";
 import Signup from "./Signup";
 
 function Routes() {
     return (
         <Router>
-            <div>
-                {/* Example navigation links (you can style or place them as you wish) */}
-                <Link to="/">Home</Link>
-                <Link to="/signup">SignUp</Link>
-
-                {/* Route setup */}
-                <Route path="/" exact component={App} />
-                <Route path="/signup" component={Signup} />
-            </div>
+                <Route path="/" element={<App/>} />
+                <Route path="/signup" element={<Signup/>} />
         </Router>
     );
 }

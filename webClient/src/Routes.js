a0d2b5d2193da0e897d182a26e7158a0670b09c6
Ilai Av-Ron <ilai.avron@post.runi.ac.il>
2023-10-10 22:25:03 +0300

    Connectivity fixed, Homepage and Singup pages complete.

--- webClient/src/Routes.js
+++ webClient/src/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes as Router, Route, Link } from "react-router-dom";
 import App from "./App";
 import Signup from "./Signup";
+import TeacherHomepage from "./TeacherHomepage";
 
 function Routes() {
     return (
         <Router>
-                <Route path="/" element={<App/>} />
-                <Route path="/signup" element={<Signup/>} />
+            <Route path="/" element={<App/>} />
+            <Route path="/signup" element={<Signup/>} />
+            <Route path="/teacher-homepage" element={<TeacherHomepage/>} /> 
         </Router>
     );
 }
--- webClient/src/Signup.js
+++ webClient/src/Signup.js
@@ -1,69 +1,213 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button, ListGroup, CloseButton } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Signup = () => {
-    const [userType, setUserType] = useState("");  
+    const [userType, setUserType] = useState("student");
+    const [subjects, setSubjects] = useState([]);
+    const [selectedSubject, setSelectedSubject] = useState('מתמטיקה');
+    const [startClass, setStartClass] = useState("א'");
+    const [endClass, setEndClass] = useState("א'");
+
+    const handleAddSubject = () => {
+        const newSubject = {
+            subject: selectedSubject,
+            range: `${startClass} עד ${endClass}`
+        };
+        setSubjects(prevSubjects => [...prevSubjects, newSubject]);
+    }
+
+    const handleRemoveSubject = (index) => {
+        const updatedSubjects = [...subjects];
+        updatedSubjects.splice(index, 1);
+        setSubjects(updatedSubjects);
+    }
 
     return (
-        <Container className="mt-5" dir="rtl">
-            <Row>
+        <Container className="mt-3" dir="rtl">
+            <Row className="mb-3">
                 <Col className="text-center">
                     <h1>הרשמה</h1>
                 </Col>
             </Row>
 
-            <Row className="mb-4" dir="rtl">
-                <Col md={{ span: 6, offset: 3 }}>
+            <Row className="justify-content-center">
+                <Col md={6}>
                     <Form>
-                        <Form.Group>
-                            <Form.Label>בחר סוג משתמש:</Form.Label>
+                        <Form.Group className="mb-4">
+                            <Form.Label className="mb-2">בחר סוג משתמש:</Form.Label>
                             <Form.Check
+                                custom
                                 type="radio"
+                                id="custom-radio-teacher"
                                 label="מורה"
                                 name="userType"
-                                id="teacher"
+                                inline
                                 onChange={() => setUserType("teacher")}
                             />
                             <Form.Check
+                                custom
                                 type="radio"
+                                id="custom-radio-student"
                                 label="תלמיד"
                                 name="userType"
-                                id="student"
+                                inline
+                                checked={userType === "student"}
                                 onChange={() => setUserType("student")}
                             />
                         </Form.Group>
 
                         {userType === "teacher" && (
-                            <>
-                                {/* Fields specific for Teacher */}
-                                <Form.Group>
-                                    <Form.Label>מקצוע:</Form.Label>
-                                    <Form.Control type="text" placeholder="הכנס את מקצוע ההוראה שלך" />
-                                </Form.Group>
-                            </>
-                        )}
+                <>
+                    <Form.Group className="mb-4">
+                        <Form.Label className="mb-2">מקצוע:</Form.Label>
+                        <Row className="mb-2">
+                            <Col md={3}>
+                                <Form.Control as="select" value={selectedSubject} onChange={(e) => setSelectedSubject(e.target.value)}>
+                                    <option value="מתמטיקה">מתמטיקה</option>
+                                    <option value="היסטוריה">היסטוריה</option>
+                                    <option value="אנגלית">אנגלית</option>
+                                    <option value="לשון">לשון</option>
+                                    <option value="ביולוגיה">ביולוגיה</option>
+                                    <option value="כימיה">פיזיקה</option>
+                                    <option value="כימיה">כימיה</option>
+                                    <option value="ערבית">ערבית</option>
+                                    <option value="תנך">תנ"ך</option>
+                                    <option value="מדעי המחשב">מדעי המחשב</option>
+                                </Form.Control>
+                            </Col>
+                            <Col md={3}>
+                                <Form.Control as="select" value={startClass} onChange={(e) => setStartClass(e.target.value)}>
+                                    <option>א'</option>
+                                    <option>ב'</option>
+                                    <option>ג'</option>
+                                    <option>ד'</option>
+                                    <option>ה'</option>
+                                    <option>ו'</option>
+                                    <option>ז'</option>
+                                    <option>ח'</option>
+                                    <option>ט'</option>
+                                    <option>י'</option>
+                                    <option>י"א</option>
+                                    <option>י"ב</option>
+                                </Form.Control>
+                            </Col>
+                            <Col md={1} className="text-center">
+                                עד
+                            </Col>
+                            <Col md={3}>
+                                <Form.Control as="select" value={endClass} onChange={(e) => setEndClass(e.target.value)}>
+                                    <option>א'</option>
+                                    <option>ב'</option>
+                                    <option>ג'</option>
+                                    <option>ד'</option>
+                                    <option>ה'</option>
+                                    <option>ו'</option>
+                                    <option>ז'</option>
+                                    <option>ח'</option>
+                                    <option>ט'</option>
+                                    <option>י'</option>
+                                    <option>י"א</option>
+                                    <option>י"ב</option>
+                                </Form.Control>
+                            </Col>
+                            <Col md={2}>
+                                <Button onClick={handleAddSubject}>הוסף</Button>
+                            </Col>
+                        </Row>
+                        <ListGroup className="mt-3">
+                            {subjects.map((item, index) => (
+                                <ListGroup.Item key={index}>
+                                    {`${item.subject} (${item.range})`}
+                                    <CloseButton size="sm" onClick={() => handleRemoveSubject(index)} />
+                                </ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    </Form.Group>
+                    <Form.Group className="mb-4">
+                        <Form.Label>גיל:</Form.Label>
+                        <Form.Control type="number" placeholder="הכנס גיל" />
+                    </Form.Group>
+                    <Form.Group className="mb-4">
+                        <Form.Label>על עצמי:</Form.Label>
+                        <Form.Control as="textarea" rows={4} placeholder="ספר על עצמך" />
+                    </Form.Group>
+                    <Form.Group className="mb-4">
+                        <Form.Label>תמונה:</Form.Label>
+                        <Form.Control type="file" accept="image/*" />
+                    </Form.Group>
+                    <Form.Group className="mb-4">
+                        <Form.Label>קישור לפרופיל חברתי:</Form.Label>
+                        <Form.Control type="url" placeholder="הכנס קישור" />
+                    </Form.Group>
+                </>
+            )}
 
                         {userType === "student" && (
-                            <>
-                                {/* Fields specific for Student */}
-                                <Form.Group>
-                                    <Form.Label>מוסד לימודים:</Form.Label>
-                                    <Form.Control type="text" placeholder="הכנס את מקום לימודיך" />
-                                </Form.Group>
-                            </>
+                            <Row>
+                                <Col md={6}>
+                                    <h4 className="mb-3">פרטי ההורה</h4>
+                                    <Form.Group className="mb-4">
+                                        <Form.Label>שם:</Form.Label>
+                                        <Form.Control type="text" placeholder="הכנס שם ההורה" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-4">
+                                        <Form.Label>שם משפחה:</Form.Label>
+                                        <Form.Control type="text" placeholder="הכנס שם משפחה" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-4">
+                                        <Form.Label>טלפון:</Form.Label>
+                                        <Form.Control type="tel" placeholder="הכנס טלפון ההורה" />
+                                    </Form.Group>
+                                </Col>
+                                <Col md={6}>
+                                    <h4 className="mb-3">פרטי הילד</h4>
+                                    <Form.Group className="mb-4">
+                                        <Form.Label>שם:</Form.Label>
+                                        <Form.Control type="text" placeholder="הכנס שם הילד" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-4">
+                                        <Form.Label>שם משפחה:</Form.Label>
+                                        <Form.Control type="text" placeholder="הכנס שם משפחה" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-4">
+                                        <Form.Label className="mb-2">כיתה:</Form.Label>
+                                        <Form.Control as="select">
+                                            <option>א'</option>
+                                            <option>ב'</option>
+                                            <option>ג'</option>
+                                            <option>ד'</option>
+                                            <option>ה'</option>
+                                            <option>ו'</option>
+                                            <option>ז'</option>
+                                            <option>ח'</option>
+                                            <option>ט'</option>
+                                            <option>י'</option>
+                                            <option>י"א</option>
+                                            <option>י"ב</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Col>
+                            </Row>
                         )}
 
-                        <Form.Group>
-                            <Form.Label>אימייל:</Form.Label>
-                            <Form.Control type="email" placeholder="הכנס אימייל" />
-                        </Form.Group>
+                        <hr className="my-4" /> {}
 
-                        <Form.Group>
-                            <Form.Label>סיסמה:</Form.Label>
-                            <Form.Control type="password" placeholder="הכנס סיסמה" />
-                        </Form.Group>
-
-                        <Button variant="primary" type="submit">
+                        <Row>
+                            <Col md={6}>
+                                <Form.Group className="mb-4">
+                                    <Form.Label className="mb-2">אימייל:</Form.Label>
+                                    <Form.Control type="email" placeholder="הכנס כתובת אימייל" />
+                                </Form.Group>
+                            </Col>
+                            <Col md={6}>
+                                <Form.Group className="mb-4">
+                                    <Form.Label className="mb-2">סיסמה:</Form.Label>
+                                    <Form.Control type="password" placeholder="הכנס סיסמה" />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Button variant="primary" type="submit" className="w-100">
                             הרשם
                         </Button>
                     </Form>

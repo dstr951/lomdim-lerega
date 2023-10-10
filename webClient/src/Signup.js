0a34dd1308257d463cf49def9e197ae357853259
Ilai Av-Ron <ilai.avron@post.runi.ac.il>
2023-10-10 23:47:32 +0300

    Added hooks and onchanges to signup.

--- webClient/src/Signup.js
+++ webClient/src/Signup.js
@@ -1,13 +1,32 @@
 import React, { useState } from "react";
 import { Container, Row, Col, Form, Button, ListGroup, CloseButton } from "react-bootstrap";
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Signup = () => {
-    const [userType, setUserType] = useState("student");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // Teacher fields
     const [subjects, setSubjects] = useState([]);
     const [selectedSubject, setSelectedSubject] = useState('מתמטיקה');
     const [startClass, setStartClass] = useState("א'");
-    const [endClass, setEndClass] = useState("א'");
+    const [endClass, setEndClass] = useState("י'ב");
+    const [age, setAge] = useState("");
+    const [socialProfileLink, setSocialProfileLink] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [profilePicture, setProfilePicture] = useState(""); // You may want to handle this differently for actual image uploads
+    const [aboutMe, setAboutMe] = useState("");
+
+    // Student fields
+    const [parentFirstName, setParentFirstName] = useState("");
+    const [parentLastName, setParentLastName] = useState("");
+    const [parentPhoneNumber, setParentPhoneNumber] = useState("");
+    const [studentFirstName, setStudentFirstName] = useState("");
+    const [studentLastName, setStudentLastName] = useState("");
+    const [studentClass, setStudentClass] = useState("א'");
+
+    const [userType, setUserType] = useState("student");
 
     const handleAddSubject = () => {
         const newSubject = {
@@ -126,19 +145,19 @@ const Signup = () => {
                     </Form.Group>
                     <Form.Group className="mb-4">
                         <Form.Label>גיל:</Form.Label>
-                        <Form.Control type="number" placeholder="הכנס גיל" />
+                        <Form.Control type="number" placeholder="הכנס גיל" value={age} onChange={(e) => setAge(e.target.value)} />
                     </Form.Group>
                     <Form.Group className="mb-4">
                         <Form.Label>על עצמי:</Form.Label>
-                        <Form.Control as="textarea" rows={4} placeholder="ספר על עצמך" />
+                        <Form.Control as="textarea" rows={4} placeholder="ספר על עצמך" value={aboutMe} onChange={(e) => setAboutMe(e.target.value)} />
                     </Form.Group>
                     <Form.Group className="mb-4">
                         <Form.Label>תמונה:</Form.Label>
-                        <Form.Control type="file" accept="image/*" />
+                        <Form.Control type="file" accept="image/*" value={profilePicture} onChange={(e) => setProfilePicture(e.target.value)} />
                     </Form.Group>
                     <Form.Group className="mb-4">
                         <Form.Label>קישור לפרופיל חברתי:</Form.Label>
-                        <Form.Control type="url" placeholder="הכנס קישור" />
+                        <Form.Control type="url" placeholder="הכנס קישור" value={socialProfileLink} onChange={(e) => setSocialProfileLink(e.target.value)} />
                     </Form.Group>
                 </>
             )}
@@ -149,30 +168,30 @@ const Signup = () => {
                                     <h4 className="mb-3">פרטי ההורה</h4>
                                     <Form.Group className="mb-4">
                                         <Form.Label>שם:</Form.Label>
-                                        <Form.Control type="text" placeholder="הכנס שם ההורה" />
+                                        <Form.Control type="text" placeholder="הכנס שם ההורה" value={parentFirstName} onChange={(e) => setParentFirstName(e.target.value)} />
                                     </Form.Group>
                                     <Form.Group className="mb-4">
                                         <Form.Label>שם משפחה:</Form.Label>
-                                        <Form.Control type="text" placeholder="הכנס שם משפחה" />
+                                        <Form.Control type="text" placeholder="הכנס שם משפחה" value={parentLastName} onChange={(e) => setParentLastName(e.target.value)} />
                                     </Form.Group>
                                     <Form.Group className="mb-4">
                                         <Form.Label>טלפון:</Form.Label>
-                                        <Form.Control type="tel" placeholder="הכנס טלפון ההורה" />
+                                        <Form.Control type="tel" placeholder="הכנס טלפון ההורה" value={parentPhoneNumber} onChange={(e) => setParentPhoneNumber(e.target.value)} />
                                     </Form.Group>
                                 </Col>
                                 <Col md={6}>
                                     <h4 className="mb-3">פרטי הילד</h4>
                                     <Form.Group className="mb-4">
                                         <Form.Label>שם:</Form.Label>
-                                        <Form.Control type="text" placeholder="הכנס שם הילד" />
+                                        <Form.Control type="text" placeholder="הכנס שם הילד" value={studentFirstName} onChange={(e) => setStudentFirstName(e.target.value)} />
                                     </Form.Group>
                                     <Form.Group className="mb-4">
                                         <Form.Label>שם משפחה:</Form.Label>
-                                        <Form.Control type="text" placeholder="הכנס שם משפחה" />
+                                        <Form.Control type="text" placeholder="הכנס שם משפחה" value={studentLastName} onChange={(e) => setStudentLastName(e.target.value)} />
                                     </Form.Group>
                                     <Form.Group className="mb-4">
                                         <Form.Label className="mb-2">כיתה:</Form.Label>
-                                        <Form.Control as="select">
+                                        <Form.Control as="select" value={studentClass} onChange={(e) => setStudentClass(e.target.value)}>
                                             <option>א'</option>
                                             <option>ב'</option>
                                             <option>ג'</option>
@@ -197,13 +216,13 @@ const Signup = () => {
                             <Col md={6}>
                                 <Form.Group className="mb-4">
                                     <Form.Label className="mb-2">אימייל:</Form.Label>
-                                    <Form.Control type="email" placeholder="הכנס כתובת אימייל" />
+                                    <Form.Control type="email" placeholder="הכנס כתובת אימייל" value={email} onChange={(e) => setEmail(e.target.value)} />
                                 </Form.Group>
                             </Col>
                             <Col md={6}>
                                 <Form.Group className="mb-4">
                                     <Form.Label className="mb-2">סיסמה:</Form.Label>
-                                    <Form.Control type="password" placeholder="הכנס סיסמה" />
+                                    <Form.Control type="password" placeholder="הכנס סיסמה" value={password} onChange={(e) => setPassword(e.target.value)} />
                                 </Form.Group>
                             </Col>
                         </Row>

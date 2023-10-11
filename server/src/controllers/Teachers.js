474f271571c493fe096e5add0d3bcd3fc2ae0d94
Reef Kenig <rkenig@aol.com>
2023-10-11 19:35:44 +0300

    added get all teachers endpoint

--- server/src/controllers/Teachers.js
+++ server/src/controllers/Teachers.js
@@ -1,20 +1,58 @@
-const TeachersService  = require("../services/Teachers");
-const bcrypt = require ('bcrypt');
+const { Teacher } = require("../models/Teachers");
+const TeachersService = require("../services/Teachers");
+const bcrypt = require("bcrypt");
 const SALT_ROUNDS = 10;
 
-async function registerTeacher(req,res){
-    const {email, password, firstName, lastName, age, socialProfileLink, phoneNumber, profilePicture, aboutMe, canTeach} = req.body
-    bcrypt.hash(password, SALT_ROUNDS, async function(err, hashedPassword) {
-        const registerTeacherResponse = await TeachersService.registerTeacher(
-            email, hashedPassword, firstName, lastName, age, socialProfileLink, phoneNumber, profilePicture, aboutMe, canTeach)
-        if(registerTeacherResponse.status == 200) {
-            res.status(200).send(registerTeacherResponse.body)
-        } else {
-            res.status(registerTeacherResponse.status).send(registerTeacherResponse.error)
-        }
-    });    
+async function registerTeacher(req, res) {
+  const {
+    email,
+    password,
+    firstName,
+    lastName,
+    age,
+    socialProfileLink,
+    phoneNumber,
+    profilePicture,
+    aboutMe,
+    canTeach,
+  } = req.body;
+  bcrypt.hash(password, SALT_ROUNDS, async function (err, hashedPassword) {
+    const registerTeacherResponse = await TeachersService.registerTeacher(
+      email,
+      hashedPassword,
+      firstName,
+      lastName,
+      age,
+      socialProfileLink,
+      phoneNumber,
+      profilePicture,
+      aboutMe,
+      canTeach
+    );
+    if (registerTeacherResponse.status == 200) {
+      res.status(200).send(registerTeacherResponse.body);
+    } else {
+      res
+        .status(registerTeacherResponse.status)
+        .send(registerTeacherResponse.error);
+    }
+  });
+}
+
+async function getAllTeachers() {
+  try {
+    const teachers = await Teacher.find({});
+    if (!teachers) {
+      return { status: 404, error: "No teachers found" };
+    }
+    return { status: 200, body: teachers };
+  } catch (err) {
+    console.log("Error: ", err);
+    return { status: 500, body: err };
+  }
 }
 
 module.exports = {
-    registerTeacher,
-}
+  registerTeacher,
+  getAllTeachers,
+};
--- server/src/routes/Teachers.js
+++ server/src/routes/Teachers.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const TeachersController = require('../controllers/Teachers')
+const express = require("express");
+const TeachersController = require("../controllers/Teachers");
 const router = express.Router();
 
-router.post('/',TeachersController.registerTeacher);
+router.post("/", TeachersController.registerTeacher);
+router.get("/api/Teachers", TeachersController.getAllTeachers);
 
 module.exports = router;

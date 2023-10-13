67506cc8255fce9a352d2851ae945e3a56a1f563
Reef Kenig <rkenig@aol.com>
2023-10-13 21:54:38 +0300

    reinitiated registration functions

--- server/src/routes/Teachers.js
+++ server/src/routes/Teachers.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const TeachersController = require("../controllers/Teachers");
-const TokensService = require("../services/Tokens")
+const TokensService = require("../services/Tokens");
 const router = express.Router();
 
-router.get('/search', TokensService.isLoggedIn, TeachersController.searchTeachers);
-router.post("/:email/approve", TokensService.isAdmin, TeachersController.approveTeacher);
-router.post("/:email/reject", TokensService.isAdmin, TeachersController.rejectTeacher);
+router.get(
+  "/search",
+  TokensService.isLoggedIn,
+  TeachersController.searchTeachers
+);
 router.get("/all", TokensService.isLoggedIn, TeachersController.getAllTeachers);
-router.get("/all/admin", TokensService.isAdmin, TeachersController.getAllTeachersAdmin);
+router.get(
+  "/all/admin",
+  TokensService.isAdmin,
+  TeachersController.getAllTeachersAdmin
+);
+router.post("/", TeachersController.registerTeacher);
+router.post(
+  "/:email/approve",
+  TokensService.isAdmin,
+  TeachersController.approveTeacher
+);
+router.post(
+  "/:email/reject",
+  TokensService.isAdmin,
+  TeachersController.rejectTeacher
+);
 
 module.exports = router;

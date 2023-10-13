a69d28b88dc58dd97b88561dec046935c2e50d24
Daniel Goldstein <dstr159@gmail.com>
2023-10-14 01:40:32 +0300

    add email services

--- server/src/services/Emailing.js
+++ server/src/services/Emailing.js
@@ -1,27 +1,59 @@
-import sgMail from '@sendgrid/mail'
-const sgMail = require('@sendgrid/mail')
+const sgMail = require("@sendgrid/mail");
+const { idToSubject, idToGrade } = require("../../../webClient/src/Converters");
 
-function notifyTeacherWasRequested(teacherEmail, studentPhoneNumber, studentEmail, studentMessage, ){
-    sgMail.setApiKey(process.env.SEND_GRIND_NOTIFY_MATCH_KEY)
-    const msg = {
+function notifyTeacherWasRequested(
+  teacherEmail,
+  studentPhoneNumber,
+  studentEmail,
+  studentMessage
+) {
+  sgMail.setApiKey(process.env.SEND_GRIND_NOTIFY_MATCH_KEY);
+  const msg = {
     to: teacherEmail,
-    from: 'lomdimlerega@gmail.com',
+    from: "lomdimlerega@gmail.com",
     dynamic_template_data: {
-        subject: `תלמיד ביקש ללמוד איתך - ${studentEmail}`,
-        content: studentMessage,
-        number: studentPhoneNumber,
-        email: studentEmail
+      subject: `תלמיד ביקש ללמוד איתך - ${studentEmail}`,
+      content: studentMessage,
+      number: studentPhoneNumber,
+      email: studentEmail,
     },
-    template_id: "d-55fdb512444844178dc3b235fb88cac8"
-    }
-    sgMail
+    template_id: "d-55fdb512444844178dc3b235fb88cac8",
+  };
+  sgMail
     .send(msg)
     .then(() => {
-        console.log('Email sent')
+      console.log("Email sent");
     })
     .catch((error) => {
-        console.error(error.response.body)
-    })
+      console.error(error.response.body);
+    });
 }
 
-module.exports={ notifyTeacherWasRequested }
+function notifyMatch(request, student, teacher) {
+  const msg = {
+    to: `${student.email}`,
+    from: "lomdimlerega@gmail.com",
+    dynamic_template_data: {
+      subject: `לומדים לרגע -נמצאה התאמה-${request._id}`,
+      parentName: `${student.parent.firstName} ${student.parent.lastName}`,
+      parentPhone: `${student.parent.phoneNumber}`,
+      studentName: `${student.student.firstName} ${student.student.lastName}`,
+      studentGrade: `${idToGrade[student.student.grade]}`,
+      studySubject: `${idToSubject[request.subject]}`,
+      studentMessage: `${request.messageContent}`,
+      teacherName: `${teacher.firstName} ${teacher.lastName}`,
+      teacherPhone: `${teacher.phoneNumber}`,
+    },
+    template_id: "d-bef71c0ae3da475c84b632c4255f3eef",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error.response.body);
+    });
+}
+
+module.exports = { notifyTeacherWasRequested, notifyMatch };

1fcc3e11cf2d5880f781761ccae381f8ea4d0e19
Daniel Goldstein <dstr159@gmail.com>
2023-10-13 14:54:23 +0300

    remove registration paths for students and teacher, now we use common user path

--- server/src/routes/Students.js
+++ server/src/routes/Students.js
@@ -3,7 +3,6 @@ const StudentsController = require('../controllers/Students')
 const TokensService = require("../services/Tokens")
 const router = express.Router();
 
-router.post('/', StudentsController.registerStudent);
 router.get('/myself', TokensService.isStudent, StudentsController.getMyselfStudent);
 
 module.exports = router;

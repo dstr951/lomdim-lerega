82d56e4436b32b2d8a2f64995a27232b8cc12644
Daniel Goldstein <dstr159@gmail.com>
2023-10-13 13:02:10 +0300

    apply changes in login for this version

--- webClient/src/Login.js
+++ webClient/src/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
+import jwt from "jwt-decode"
 import './style/App.css';
 import Header from "./component/Header";
 
@@ -20,7 +21,7 @@ const Login = () => {
 
     const handleLogin = async (event) => {
         event.preventDefault();
-    
+        let token;
         try {
             const loginResponse = await axios.post(`${SERVER_ADDRESS}/api/Users/login`, {
                 email,
@@ -28,23 +29,42 @@ const Login = () => {
             });
             
             if (loginResponse.data) {
-                const token = loginResponse.data;
-                const teacherResponse = await axios.get(`${SERVER_ADDRESS}/api/Teachers/search?email=${email}`,
-                 { headers: { Authorization: token} });
-                
-                if (teacherResponse.data) {
-                    const teacher = teacherResponse.data;
-                    navigate('/teacher-homepage', { state: { teacher } });
-                } else {
-                    alert('Failed to fetch teacher data.');
+                token = loginResponse.data.token;
+                const login = jwt(token);
+                switch(login.role){
+                    case "teacher":
+                        const teacherResponse = await axios.get(`${SERVER_ADDRESS}/api/Teachers/search?email=${email}`,
+                        { headers: { Authorization: token} });                        
+                        if (teacherResponse.data) {
+                            const teacher = teacherResponse.data;
+                            navigate('/teacher-homepage', { state: { teacher, token } });
+                        }                        
+                        break;
+                    case "student":
+                        navigate('/seek-teachers', {state: {token}});
+                        break;
+                    case "admin":
+                        navigate('/admin/panel', {state: {token}});
+                        break;
+                    default:
+                        alert("there was an error")
+                        break;
                 }
-            } else {
-                alert('Failed to login.');
-            }
+            } 
         } catch (error) {
+            if(error.response?.data === "couldn't find a user with those credetials"){
+                alert('Failed to login.');
+                console.error('Error:', error);
+            } else if(error.response?.data === "We couldn't find a teacher with this email"){
+                
+            }
+            else {
+                alert("there was an error logging in")
+            }
             console.error('Error:', error);
         }
     };
+
 
     return (
         <div>

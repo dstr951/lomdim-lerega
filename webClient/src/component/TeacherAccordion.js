666cfbb6befb18c44af964be236070c93abc9926
Yarden <Yardenrsk@gmail.com>
2023-10-14 23:56:38 +0300

    Fully working accordion with Ofek's design.

--- webClient/src/component/TeacherAccordion.js
+++ webClient/src/component/TeacherAccordion.js
@@ -1,100 +1,93 @@
 import React, { useState } from "react";
-import {
-  Accordion,
-  Container,
-  Row,
-  Col,
-  ListGroup,
-  Button,
-  Image,
-} from "react-bootstrap";
+import { Image } from "react-bootstrap";
 import { idToGrade, idToSubject } from "../Converters";
 import ContactTeacherModal from "../component/ContactTeacherModal";
+import "../style/TeachersListing.css";
+import { Link } from "react-router-dom";
 
 const TeacherAccordion = ({ filteredTeachers, token }) => {
   const [modalShow, setModalShow] = React.useState(false);
   const [selectedTeacher, setSelectedTeacher] = React.useState({});
-  return (
-    <Container>
-      <Accordion data-testid={"teacherListing-list"} defaultActiveKey="0">
-        {filteredTeachers?.map((teacher) => (
-          <Accordion.Item key={teacher.email} eventKey={teacher.email}>
-            <Accordion.Header>
-              <Container>
-                <Row xs="auto">
-                  <Col>
-                    <Image
-                      src={`data:image/jpeg;base64,${teacher.profilePicture}`}
-                      roundedCircle
-                      width={100}
-                      height={100}
-                      className="mb-3"
-                    />
-                  </Col>
-                  <Col>
-                    <Row xs="auto">
-                      <Col>
-                        <h3>
-                          {teacher.firstName} {teacher.lastName}{" "}
-                        </h3>
-                      </Col>
-                      <Col>
-                        <Button
-                          href={teacher.socialProfileLink}
-                          onClick={() => onClickOpenVacancy(id)}
-                        >
-                          {"קישור לפרופיל חברתי"}
-                        </Button>
-                      </Col>
-                      {/* {teacher.isOnline ? <p>מחובר</p> : <p> לא מחובר</p>} */}
-                    </Row>
-                    <Row>
-                      <Button
-                        variant="primary"
-                        onClick={() => {
-                          setSelectedTeacher({ teacher });
-                          setModalShow(true);
-                        }}
-                      >
-                        פנה למורה
-                      </Button>
-                    </Row>
-                  </Col>
-                </Row>
-              </Container>
-            </Accordion.Header>
-            <Accordion.Body>
-              <Container>
-                <Row>
-                  {teacher.aboutMe}
+  const [activeSection, setActiveSection] = React.useState(null);
 
-                  <Row xs="auto">
-                    <Col xs>
-                      <ListGroup variant="flush">
-                        {teacher.canTeach.map((item, index) => (
-                          <ListGroup.Item key={index}>
-                            {idToSubject[item.subject]} (כיתות{" "}
-                            {idToGrade[item.lowerGrade]} עד{" "}
-                            {idToGrade[item.higherGrade]})
-                          </ListGroup.Item>
-                        ))}
-                      </ListGroup>
-                    </Col>
-                  </Row>
-                </Row>
-                <Row xs="auto"></Row>
-              </Container>
-            </Accordion.Body>
-          </Accordion.Item>
-        ))}
-      </Accordion>
+  const toggleSection = (index) => {
+    if (index === activeSection) {
+      setActiveSection(null);
+    } else {
+      setActiveSection(index);
+    }
+  };
+
+  return (
+    <div className="accordion-list">
+      {filteredTeachers?.map((teacher) => (
+        <div
+          eventKey={teacher.email}
+          key={teacher.email}
+          className="accordion"
+          onClick={() => toggleSection(teacher.email)}
+          id={
+            teacher.email === activeSection
+              ? "accordion-activate"
+              : "accordion-deactivate"
+          }
+        >
+          <div className="accordion-header">
+            <div id="image-div">
+              <Image
+                src={`data:image/jpeg;base64,${teacher.profilePicture}`}
+                roundedCircle
+                width={100}
+                height={100}
+                className="mb-3"
+              />
+            </div>
+            <div>
+              <h2 id="personal-title">
+                {teacher.firstName} {teacher.lastName}{" "}
+              </h2>
+              <div className="row-container">
+                <Link to={teacher.socialProfileLink} target="_blank">
+                  <button>{"קישור לפרופיל חברתי"}</button>
+                </Link>
+                <button
+                  variant="primary"
+                  onClick={() => {
+                    setSelectedTeacher({ teacher });
+                    setModalShow(true);
+                  }}
+                >
+                  פנה למורה
+                </button>
+              </div>
+              {teacher.email === activeSection && (
+                <div className="accordion-body">
+                  <p>
+                    {" "}
+                    <strong>קצת עלי:</strong> {teacher.aboutMe}
+                  </p>
+                  <div variant="flush" className="list">
+                    {teacher?.canTeach?.map((item, index) => (
+                      <div key={index} className="item">
+                        {idToSubject[item.subject]} (כיתות{" "}
+                        {idToGrade[item.lowerGrade]} עד{" "}
+                        {idToGrade[item.higherGrade]})
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      ))}
       <ContactTeacherModal
         show={modalShow}
         onHide={() => setModalShow(false)}
         teacher={selectedTeacher}
         token={token}
       />
-    </Container>
+    </div>
   );
 };
 export default TeacherAccordion;

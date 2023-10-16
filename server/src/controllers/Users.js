fce672fe4409e7494f7e6358726506731dba3646
Daniel Goldstein <dstr159@gmail.com>
2023-10-16 09:31:05 +0300

    change function

--- server/src/controllers/Users.js
+++ server/src/controllers/Users.js
@@ -5,7 +5,7 @@ async function loginUser(req, res) {
   //todo: refactor the db to user table with role based on the role choose the correct service
   const userLoginResponse = await UserService.loginUser(email, password);
   if (userLoginResponse.status === 200) {
-    res.set("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).send(userLoginResponse.body);
     return;
   } else if (userLoginResponse.status === 404) {

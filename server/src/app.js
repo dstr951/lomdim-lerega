bc40cc94b674bed1b1f6fe6154c7d96df71fe891
Daniel Goldstein <dstr159@gmail.com>
2023-10-16 09:35:36 +0300

    add header for all routes

--- server/src/app.js
+++ server/src/app.js
@@ -30,6 +30,10 @@ mongoose
   .then(() => {
     console.log("Connected to MongoDB");
     app.use(cors());
+    app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      next();
+    });
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(express.json({ limit: "1000mb" }));
     app.use("/api/Users", routerUsers);

19e488b04cd5816437c276f54afcb14b4b241101
Daniel Goldstein <dstr159@gmail.com>
2023-10-16 09:21:29 +0300

    add Access-Control-Allow-Origin:*

--- server/src/controllers/Users.js
+++ server/src/controllers/Users.js
@@ -1,21 +1,22 @@
-const UserService  = require("../services/Users");
+const UserService = require("../services/Users");
 
 async function loginUser(req, res) {
-    const {email, password} = req.body
-    //todo: refactor the db to user table with role based on the role choose the correct service
-    const userLoginResponse = await UserService.loginUser(email, password)
-    if(userLoginResponse.status === 200) {
-        res.status(200).send(userLoginResponse.body);
-        return;
-    } else if(userLoginResponse.status === 404) {
-        res.status(404).send(userLoginResponse.error);
-        return;
-    } else {
-        res.status(userLoginResponse.status).send(userLoginResponse.error);
-        return;
-    }
+  const { email, password } = req.body;
+  //todo: refactor the db to user table with role based on the role choose the correct service
+  const userLoginResponse = await UserService.loginUser(email, password);
+  if (userLoginResponse.status === 200) {
+    res.set("Access-Control-Allow-Origin", "*");
+    res.status(200).send(userLoginResponse.body);
+    return;
+  } else if (userLoginResponse.status === 404) {
+    res.status(404).send(userLoginResponse.error);
+    return;
+  } else {
+    res.status(userLoginResponse.status).send(userLoginResponse.error);
+    return;
+  }
 }
 
 module.exports = {
-    loginUser,
-}
+  loginUser,
+};

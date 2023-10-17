d8bb8c6a7b5fd5484e57e2be7d612c25dbf4ee6a
Daniel Goldstein <dstr159@gmail.com>
2023-10-17 18:55:44 +0300

    add admin validation

--- server/src/routes/Students.js
+++ server/src/routes/Students.js
@@ -4,25 +4,25 @@ const TokensService = require("../services/Tokens");
 const TeachersController = require("../controllers/Teachers");
 const router = express.Router();
 
-router.get(
-  "/myself",
-  StudentsController.getMyselfStudent
-);
+router.get("/myself", StudentsController.getMyselfStudent);
 
 router.get(
-    "/all/admin",
-    StudentsController.getAllStudentsAdmin
+  "/all/admin",
+  TokensService.isAdmin,
+  StudentsController.getAllStudentsAdmin
 );
 
 router.post("/", StudentsController.registerStudent);
 
 router.post(
   "/:email/approve",
+  TokensService.isAdmin,
   StudentsController.approveStudent
 );
 
 router.post(
   "/:email/reject",
+  TokensService.isAdmin,
   StudentsController.rejectStudent
 );
 
